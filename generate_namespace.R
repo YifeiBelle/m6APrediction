54154541531990ee6373b3627d35ca5779b09614
YifeiBelle <yifeibelle@gmail.com>
2025-11-14 23:04:03 +0800

    Clean up project by removing unnecessary files

--- generate_namespace.R
+++
@@ -1,6 +0,0 @@
-# This script generates the NAMESPACE file from roxygen2 comments.
-# This is a necessary step before building and installing the package.
-if (!requireNamespace("roxygen2", quietly = TRUE)) {
-  stop("The 'roxygen2' package is required but not installed. Please install it by running: install.packages('roxygen2')")
-}
-roxygen2::roxygenise()
